63567c04ce0d9344313ce208a42276567a0cb5e2
Andrew <andrewleigh6@gmail.com>
2019-08-29 10:22:09 +0100

    Remove console log from nav js

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,8 +1,6 @@
 const navCheckbox = document.getElementById("toggle-navigation");
 const navItems = document.getElementsByClassName("navigation__item");
 
-console.log(navItems);
-
 for (let i = 0; i < navItems.length; i++) {
   navItems[i].addEventListener("click", function() {
     navCheckbox.checked = false;
